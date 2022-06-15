46c204191dc4af02319550fd4bf1d37d8a14d310
Brian Wangora <brianwangora@gmail.com>
2022-06-15 20:45:41 +0300

    tasks complete

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const init = () => {
 
         console.log(input.value);
 
-        fetch(`http://localhost:3000/movies`)
+        fetch(`http://localhost:3000/movies/${input.value}`)
         .then(response => response.json())
         .then(data => {
         const title = document.querySelector('section#movieDetails h4');
